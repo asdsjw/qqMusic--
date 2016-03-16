cefa07870cc32a73749ccad1ed09b6917ae74f87
asdsjw <sunjunwei@sunjunweidemacbook-pro.local>
2016-03-16 14:58:29 +0800

    Initial Commit

---
+++ qqMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qqMusic
+//
+//  Created by Sunjunwei on 16/3/16.
+//  Copyright © 2016年 sjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qqMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qqMusic
+//
+//  Created by Sunjunwei on 16/3/16.
+//  Copyright © 2016年 sjw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
